3896555ad845bf7836712881c6ef8f181e2aad0b
sohelranaweb <sohel.rana.web2@gmail.com>
2026-02-01 12:54:33 +0600

    deploy

--- src/lib/server-fetch.ts
+++ src/lib/server-fetch.ts
@@ -1,7 +1,6 @@
 import { getCookie } from "@/services/auth/takenHandlers";
 
-const BACKEND_API_URL =
-  process.env.NEXT_PUBLIC_BASE_API_URL || "http://localhost:5000/api/v1";
+const BACKEND_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 
 const serverFetchHelper = async (
   endpoint: string,
